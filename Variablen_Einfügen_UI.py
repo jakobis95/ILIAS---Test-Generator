171bbc794e082ddda9d8c3d4550bbc0b860265df
jakobis95 <70392662+jakobis95@users.noreply.github.com>
2021-04-27 19:11:28 +0200

    Ausführliche Kommentierung hinzugefügt

--- Variablen_Einfügen_UI.py
+++ Variablen_Einfügen_UI.py
@@ -1,20 +1,23 @@
 from tkinter import *
 from tkinter import ttk
 from Picture_interface import picture_choice
-import tkinter.font as font
-import tkinter as tk
 
 
 
 class variable_scrl_UI():
     def __init__(self, varname_list, bg_color, label_color, Label_Font, frame, StringVarList, index_dict, WIDTH, Rows, Columns, Header, header_index, column_type_list, columnwidth):
+        #hintergrundfarbe
         self.bg_color = bg_color
         self.label_color = label_color
         self.Label_Font = Label_Font
         self.Rows = Rows
         self.Columns = Columns
+        #liste der relativen spaltenbreite in anzahl sections
         self.columnwidth = columnwidth
+
         self.StringVarList = StringVarList
+
+        #frame für Überschriften
         HeadingFrame = Frame(frame)
         HeadingFrame.place(relx=0, rely=0, relwidth=1, relheight=.2)
 
@@ -42,39 +45,43 @@ class variable_scrl_UI():
         Var_list = []
         rowlist = []
 
-        # Screeninfo
+        # Screeninfo anzahl sections berechnen und daraus die relative breite einer section
+        # wird später für die Breite der Spalten verwendet
         sections = 0
         for column in self.columnwidth:
             sections = sections + column
-
         relwidth = WIDTH / sections
 
-        for row in range(self.Rows):
-            rowlist.append(choice_row(index_dict, column_type_list, self.StringVarList, second_frame, row, relwidth , varname_list, self.Columns, self.columnwidth))
-
-        HeadingFrame = Frame(frame)
-        HeadingFrame.place(relx=0, rely=0, relwidth=1, relheight=.2)
-
-
-
-
+        # erstellt überschriften für jede Spalte
         self.varlist = []
         self.var_name_label = []
-        Y_abstand = .1
-
-
-        self.var_header_label = Label(HeadingFrame, text=Header, bg=self.label_color, fg=self.bg_color)
-        self.var_header_label['font'] = self.Label_Font
-        self.var_header_label.place(relx=0, rely=0, relwidth=1, relheight=.5)
         relx = 0
         relw = 0
+
         for column in range(Columns):
-            print(header_index[column])
-            relw = columnwidth[column] / sections # relative breite der spaltenheader ist gleich der Auteilung der gesamtbreite (sections) durch die anzahl der sections für diese Spalte
-            self.var_name_label.append(Label(HeadingFrame, text=header_index[column], bg=self.label_color, fg=self.bg_color))
+            # Breite der einzelnen spalten setzt sich aus der Anzahl der sections einer Spalte mal die relwidth zusammen
+            relw = columnwidth[column] / sections
+            self.var_name_label.append(
+                Label(HeadingFrame, text=header_index[column], bg=self.label_color, fg=self.bg_color))
             self.var_name_label[column]['font'] = self.Label_Font
             self.var_name_label[column].place(relx=relx, rely=0.5, relwidth=relw, relheight=.5)
             relx = relw + relx
+
+        # erstellt eine Reihe die widerum die einnzelnen Spalten erstellt
+        # alle reihen werden in einer liste gespeicher für einfachen zugriff bei erweiterung des Funktionsumfangs
+        for row in range(self.Rows):
+            rowlist.append(choice_row(index_dict, column_type_list, self.StringVarList, second_frame, row, relwidth , varname_list, self.Columns, self.columnwidth))
+
+        # erstellt Frame für gesamtüberschrift und Spaltenüberschriften
+        HeadingFrame = Frame(frame)
+        HeadingFrame.place(relx=0, rely=0, relwidth=1, relheight=.2)
+
+        # erstellt gesamtüberschrift für Scrollblock
+        self.var_header_label = Label(HeadingFrame, text=Header, bg=self.label_color, fg=self.bg_color)
+        self.var_header_label['font'] = self.Label_Font
+        self.var_header_label.place(relx=0, rely=0, relwidth=1, relheight=.5)
+
+
 
 class choice_column():
     def __init__(self, column_type, VarFrame, row, column, width,  TextVar):
@@ -99,10 +106,9 @@ class choice_row():
         self.columns = columns
         self.rowlist = []
         self.Var_list = Var_list
-        print("Varlist:", Var_list[19][0])
+
         for column in range(self.columns):
             index = index_dict[varname_list[column] .format(row + 1)]
-            print(index)
             self.rowlist.append(choice_column(column_type[column], second_frame, row, column, int(relwidth * columnwidth[column]), Var_list[index][0]))
 
 
